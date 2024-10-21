c7b595bd59fb4fe71d24b042f92e3868b0d8e1d9
asikharulidze <ana.sixarulidze@gmail.com>
2024-10-21 11:10:11 +0400

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the year of birth:");
+        int birthYear = int.Parse(Console.ReadLine());
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+        Console.WriteLine("Your age is: " + age);
+    }
+}
